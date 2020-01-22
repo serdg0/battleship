cce5fefd3500b6eaeb4026efe58e7f8700cd3c93
serdg0 <sergio.diazg92@gmail.com>
2020-01-22 10:08:18 -0600

    init development: gameboard and ship

---
+++ src/Gameboard.js
@@ -0,0 +1,38 @@
+const Coordinate = ({ship = null, hit = false} = {}) => ({
+    ship,
+    hit,
+})
+
+const Gameboard = ( { ships = ships, grid = Array.from(Array(10) , x => Array.from(Array(10), x => Coordinate())) } = {} ) => ({
+    hits: 0,
+    sunkShips: 0,
+    ships,
+    grid,
+    putShip(ship, x, y) {
+        let pos = ship.length;
+        while (pos > 0) {
+            this.grid[x][y].ship = ship;
+            pos -= 1;
+            y += 1;
+        }
+        return this;
+    },
+    receiveAttack(x, y) {
+        const coord = this.grid[x][y];
+        if (coord.hit === false) {
+            coord.hit = true;
+            this.hits += 1;
+        }
+        if (coord.ship != null) {
+            coord.ship.hit();
+            if (coord.ship.isSunk()) {
+                this.sunkShips += 1;
+            }
+        }
+    },
+    AllShipsDown() {
+        return this.ships.length == this.sunkShips;
+    }
+})
+
+export default Gameboard;
---
+++ src/Ship.js
@@ -0,0 +1,13 @@
+const Ship = ({length, name}) => ({
+    length,
+    name,
+    hit() {
+        this.length -= 1;
+        return this;
+    },
+    isSunk() {
+        return this.length == 0
+    },
+})
+
+export default Ship;
