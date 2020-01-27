de4fb0f391c149e654fb40d948a316ff43259e8f
eapenzacharias <eapenzac@gmail.com>
2020-01-27 21:39:39 +0100

    UI changes with player board trial

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,4 @@
-function renderBoard(game) {
+function renderPBoard(game) {
   const boardOne = document.createElement('table');
   const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -32,9 +32,42 @@ function renderBoard(game) {
   return boardOne;
 }
 
+function renderEBoard(game) {
+  const boardOne = document.createElement('table');
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  function printRow(row) {
+    const rowTab = document.createElement('tr');
+
+    function printEle(col) {
+      const cell = document.createElement('td');
+      const button = document.createElement('button');
+      button.innerHTML = `${row}${col}`;
+      button.setAttribute('id', `a${row}${col}`);
+      button.onclick = function() {
+        game.receiveAttack(row, col);
+        if (game.grid[row][col].ship != null) {
+          button.setAttribute('class', 'ship');
+        } else {
+          button.innerHTML = '*';
+          button.setAttribute('class', 'pressed');
+        }
+        button.onclick = null;
+      };
+      cell.appendChild(button);
+      rowTab.appendChild(cell);
+    }
+    arr.forEach(printEle);
+    boardOne.appendChild(rowTab);
+  }
+  arr.forEach(printRow);
+  return boardOne;
+}
+
 function dispInit(board1, board2) {
-  document.getElementById('board-1').appendChild(renderBoard(board1));
-  document.getElementById('board-2').appendChild(renderBoard(board2));
+  document.getElementById('board-1').appendChild(renderPBoard(board1));
+  document.getElementById('board-2').appendChild(renderEBoard(board2));
+  document.getElementById('board-3').appendChild(renderEBoard(board1));
 }
 
 export default dispInit;
