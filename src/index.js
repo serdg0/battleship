621ea67da24c699645d687b5f4e94cf675e816ea
serdg0 <sergio.diazg92@gmail.com>
2020-01-28 17:19:25 -0600

    pirate ships javascript great idea of referencing to the same memory bit SOLVED

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import dispInit from './dom';
 import Player from './Player';
 import './assets/css/style.css';
 
-
-
-
-
-
 //compPosition(computer.board);
 
 
@@ -19,8 +14,17 @@ function gameStarter() {
   const patrolBoat = Ship({ name: 'patrolBoat', length: 2 });
 
   const ships = [carrier, battleship, submarine, destroyer, patrolBoat];
+
+  const compcarrier = Ship({ name: 'carrier', length: 5 });
+  const compbattleship = Ship({ name: 'battleship', length: 4 });
+  const compsubmarine = Ship({ name: 'submarine', length: 3 });
+  const compdestroyer = Ship({ name: 'destroyer', length: 3 });
+  const comppatrolBoat = Ship({ name: 'patrolBoat', length: 2 });
+
+  const compships = [compcarrier, compbattleship, compsubmarine, compdestroyer, comppatrolBoat];
+
   const player = Player({ id: 0, name: 'Player', ships: ships });
-  const computer = Player({ id: 1, name: 'Evil machine', ships: ships });
+  const computer = Player({ id: 1, name: 'Evil machine', ships: compships });
   dispInit(player, computer);
 }
 
